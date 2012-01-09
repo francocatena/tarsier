3302c1423ad021c4baeff83aaeb2b47b278eef19
Franco Catena <francocatena@gmail.com>
2012-01-08 23:50:50 -0300

    Initial base code

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.0.rc2'
+
+gem 'pg'
+gem 'jquery-rails'
+gem 'will_paginate'
+gem 'capistrano'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'therubyracer', require: false
+  gem 'uglifier'
+end
+
+group :development do
+  gem 'ruby-debug19', require: 'ruby-debug'
+  gem 'mongrel', '1.2.0.pre2'
+end
+
+group :test do
+  gem 'turn', require: false
+  gem 'minitest', require: false
+  gem 'capybara', require: false
+  gem 'database_cleaner' # For Capybara
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,40 @@
+ENV["RAILS_ENV"] = "test"
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+require 'capybara/rails'
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
+
+# Transactional fixtures do not work with Selenium tests, because Capybara
+# uses a separate server thread, which the transactions would be hidden
+# from. We hence use DatabaseCleaner to truncate our test database.
+DatabaseCleaner.strategy = :truncation
+
+class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
+
+  # Stop ActiveRecord from wrapping tests in transactions
+  self.use_transactional_fixtures = false
+
+  teardown do
+    # Truncate the database
+    DatabaseCleaner.clean
+    # Forget the (simulated) browser state
+    Capybara.reset_sessions!
+    # Revert Capybara.current_driver to Capybara.default_driver
+    Capybara.use_default_driver
+  end
+  
+  def assert_page_has_no_errors!
+    assert !page.has_css?('#error_body')
+  end
+end
